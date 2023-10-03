66bcf204921f8441a0fe921db29f25570f62c632
Peter Li <Peter.xli@outlook.com>
2023-10-03 00:24:00 -0600

    wip

--- src/Components/AppMain/AppMain.jsx
+++ src/Components/AppMain/AppMain.jsx
@@ -4,17 +4,17 @@ import React from 'react';
 const AppMain = () => {
     return (
         <Grid container className="main-content-container">
-            <Grid item xs={12} sx={{ border: "1px solid red", height: "10vh" }}>
+            <Grid item xs={12} sx={{ border: "1px solid red", minHeight: "10vh" }}>
                 <Stack></Stack>
             </Grid>
-            <Grid item xs={12} sx={{ border: "3px solid green", height: "90vh" }}>
+            <Grid item xs={12} sx={{ border: "3px solid green", minHeight: "90vh", overflow: "auto" }}>
                 <Grid container sx={{ height: '100%' }}>
                     <Grid item xs={1} sx={{ border: "3px solid blue" }}> </Grid>
                     <Grid item xs={11} sx={{ border: "3px solid yellow" }}>
                         <Grid container sx={{ height: '100%' }}>
-                            <Grid item xs={12} sx={{ border: "1px solid red", height: '5%' }}></Grid>
-                            <Grid item xs={12} sx={{ border: "1px solid red", height: '5%' }}></Grid>
-                            <Grid item xs={12} sx={{ border: "1px solid red", height: '90%' }}></Grid>
+                            <Grid item xs={12} sx={{ border: "1px solid red", height: '8%' }}></Grid>
+                            <Grid item xs={12} gap={1} sx={{ border: "1px solid red", height: '8%' }}></Grid>
+                            <Grid item xs={12} gap={1} sx={{ border: "1px solid red", height: '84%' }}></Grid>
                         </Grid>
                     </Grid>
                 </Grid>
